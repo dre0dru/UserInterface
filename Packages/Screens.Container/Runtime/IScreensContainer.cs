46077799cae118b86de9b2076f35cce2ccd58ce5
Dre0Dru <dre0druofficial@gmail.com>
2021-11-17 09:48:32 +0300

    chore: added todo

--- Packages/Screens.Container/Runtime/IScreensContainer.cs
+++ Packages/Screens.Container/Runtime/IScreensContainer.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 
 namespace Screens.Container
 {
+    //TODO separate interface for full async api with Async Source supported
+    //Async source is either addressables or async wrapper around existing sources?
     public interface IScreensContainer<TScreenKey, TScreenConstraint>
     {
         IEnumerable<TScreenKey> Keys { get; }
