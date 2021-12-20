5145c6d471ae22cb654b1440de75047f256b19fc
Dre0Dru <dre0druofficial@gmail.com>
2021-12-20 11:29:01 +0300

    feat: removed button component requirement on press animation

--- Runtime/Animations/Common/ButtonPressAnimation.cs
+++ Runtime/Animations/Common/ButtonPressAnimation.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 namespace Screens.Animations.Common
 {
-    [RequireComponent(typeof(Button))]
     public class ButtonPressAnimation : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         [SerializeField]
